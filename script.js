7a69719efc79967ae62a6a5ecdd6920bf787d270
lucasgt2007 <lucastobarra@gmail.com>
2024-03-20 13:46:09 +0100

    Agafem text del formulari

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("holaaaaa!!!");
+function saluda() {
+    console.log("Hola!!!! Estoy vivoooo");
+    
+    // crear una variable que es diu x
+    // li dona valor
+    const x = "patata444444";
+
+    console.log(x);
+
+    const input = document.getElementById("name");
+    const inputValue = input.value;
+    console.log(inputValue);
+
+    const text2 = document.getElementById("text2");
+    document.getElementById("text2").innerHTML = inputValue;
+}
+
+function passsarAASCII() {
+
+}
