ceb3ac09aa5aa813cdb6072503142caf8af9a349
lucasgt2007 <164157753+lucasgt2007@users.noreply.github.com>
2024-04-18 11:33:45 +0200

    Variable N Multi p*q

--- script.js
+++ script.js
@@ -16,11 +16,51 @@ function saluda() {
     const input2 = document.getElementById("name").value;
     const asciiCode = input2.charCodeAt(0);
     document.getElementById("output").textContent = `ASCII code: ${asciiCode}`;
+}
+
+function test_prime(n) {
+
+  // Check if the number is equal to 1, which is not a prime number
+  if (n === 1) {
+    return true;
   }
+  // Check if the number is equal to 2, which is a prime number
+  else if (n === 2) {
+    return true;
+  } else {
+    // Iterate from 2 to n-1 to check for factors of n
+    for (var x = 2; x < n; x++) {
+      // If n is divisible by x without a remainder, it is not a prime number
+      if (n % x === 0) {
+        return false;
+      }
+    }
+    // If no factors are found, the number is a prime number
+    return true;  
+  }
+}
 
-const p = document.getElementById('p'); //p
+// Log the result of calling test_prime with the input number 37 to the console
+console.log(test_prime(37));
 
-function findN(){
-  const pValue = document.getElementById('p');
-  console.log("holaaaa----", p);
+//const p = document.getElementById('p'); //p
+//console.log(p);
+
+function findN() {
+  const p = document.getElementById('p').value;
+  const q = document.getElementById('q').value;
+
+  console.log("p primer-->", test_prime(p));
+  console.log("q primer-->", test_prime(q));
+
+  // p primer i q primer
+  if (test_prime(p) && test_prime(q)) {
+    console.log("holaaaa----", p);
+    
+    const n = p * q;
+    console.log("nnn", p, q, n)
+    
+    document.getElementById('n').value = n;
+  }
+  //
 }
