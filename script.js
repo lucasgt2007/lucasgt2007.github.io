aeac524dd07b73aadfb49ed619d8144010a982e9
lucasgt2007 <164157753+lucasgt2007@users.noreply.github.com>
2024-06-11 11:34:14 +0200

    Fet les claus generades-11/06

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-function saluda() {
-    console.log("Hola!!!! Estoy vivoooo");
-    
-    // crear una variable que es diu x
-    // li dona valor
-    const x = "patata444444";
 
-    console.log(x);
 
-    const input = document.getElementById("name");
-    const inputValue = input.value;
-    
-    console.log(inputValue);
-    const text2 = document.getElementById("text2");
-    document.getElementById("text2").innerHTML = inputValue;
-    const input2 = document.getElementById("name").value;
-    const asciiCode = input2.charCodeAt(0);
-    document.getElementById("output").textContent = `ASCII code: ${asciiCode}`;
+function textToASCII(text) {
+  const ascii= text.charCodeAt();
+  //const llargadaText = text.length;
+  //console.log("llargada--->", llargadaText);
+  //document.getElementById("textenascii").value = ascii;
+  return ascii;
 }
+
+function catchchar () {
+  const character = document.getElementById('textperencriptar').value;
+  const characterASCII = textToASCII (character);
+  //console.log(characterASCII);
+  document.getElementById("textenascii").value = characterASCII;
+}
+
+
+
+
+function AsciiTotext (ascii_code) {
+const char = String.fromCharCode (ascii_code); 
+console.log (char);
+return char;
+}
+AsciiTotext (72);
+AsciiTotext (77);
+
+function calculaN (p,q){
+  return p*q;
+}
+
 
 function test_prime(n) {
 
@@ -47,34 +60,72 @@ console.log(test_prime(37));
 //console.log(p);
 
 function findN() {
-  const p = document.getElementById('p').value;
-  const q = document.getElementById('q').value;
+  const pa = document.getElementById('p').value;
+  const qa = document.getElementById('q').value;
 
   console.log("p primer-->", test_prime(p));
   console.log("q primer-->", test_prime(q));
 
   // p primer i q primer
   if (test_prime(p) && test_prime(q)) {
-    console.log("holaaaa----", p);
     
-    const n = p * q;
-    console.log("nnn", p, q, n)
+    const n = calculaN(pa,qa);
+    //console.log("nnn", p, q, n)
     
     document.getElementById('n').value = n;
   } else {
     // error
   }
-  //
+  // funcio que calculi phi (n) = (p-1) * (q-1)
+  //llegir p,q - calculi phi - escriu phi en un input HTML.
 }
 
-function textToASCII(text) {
-  const x = text.charCodeAt(0);
-  console.log(x);
-
-  const llargadaText = text.length;
-  console.log("llargada--->", llargadaText);
+function calculaPhi (number){
+  const pa = document.getElementById('p').value;
+  const qa = document.getElementById('q').value;
+  const phi = (pa-1) * (qa-1);
+  document.getElementById('Phi').value = phi;
+  return (calculaPhi);
 }
 
-textToASCII("Hola");
 
-textToASCII("Marc i Lucas");
+ function gcd(a,b) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+    if (b > a) {
+      var temp = a; 
+      a = b;
+      b = temp;
+    }
+    while (true) {
+      console.log(a,b);
+        if (b == 0) return a;
+        a = a%b; //residu de a entre b
+        if (a == 0) return b;
+        b = b%a; //residu de b entre a
+    }
+}
+
+
+function generarE() {
+  const phi = parseInt(document.getElementById('Phi').value);
+  let e = phi;
+  console.log(e,phi);
+  console.log(gcd(e, phi));
+  while (gcd(e, phi) != 1) {
+    e -= 1;
+  }  
+  document.getElementById('e').value = e;
+}
+
+
+function generarD() {
+  var e = document.getElementById ('n').value;
+  const phi = document.getElementById ('Phi').value;
+  e = e%phi;
+    for (var x = 1; x < phi; x++) {
+        if ((e*x)%phi == 1) {
+          document.getElementById('d').value = x;
+        }
+    }
+}
